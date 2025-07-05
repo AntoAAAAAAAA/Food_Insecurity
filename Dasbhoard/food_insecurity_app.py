6eef3cb754428b9bd16a1fe5a5f5e25afcec4340
Anto <anto2005antony@gmail.com>
2025-07-05 11:42:13 -0500

    update

--- Dasbhoard/food_insecurity_app.py
+++ Dasbhoard/food_insecurity_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
-from data_loader import maybe_download_visuals
 
-#set up files 
-maybe_download_visuals(st.sidebar.checkbox("Download visuals.ipynb?", value=False))
-
-#load pages 
+# MUST be the first Streamlit command
 st.set_page_config(
     page_title="Texas Food Insecurity Dashboard",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
+from data_loader import maybe_download_visuals
+
+# Set up files AFTER set_page_config
+maybe_download_visuals(st.sidebar.checkbox("Download visuals.ipynb?", value=False))
+
+# Load pages
 home_page = st.Page(page='home_page.py', title='Home Page')
 page_2 = st.Page(page='page_2.py', title='Interactive Map')
 page_3 = st.Page(page='page_3.py', title='Visuals')
